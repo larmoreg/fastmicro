f1b271d2fbd1c8aa4e0e45b8dc558a766078d3b7
larmoreg <larmoreg@gmail.com>
2021-10-06 20:53:43 -0600

    Adding performance test for process

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -8,6 +8,7 @@ from typing import Type
 from uuid import UUID, uuid4
 
 from fastmicro.entrypoint import Entrypoint
+from fastmicro.messaging import MessagingABC, TopicABC
 from fastmicro.serializer import SerializerABC
 
 from .conftest import User, Greeting
@@ -64,9 +65,7 @@ async def test_serializer_performance(serializer_type: Type[SerializerABC]) -> N
 
 
 @pytest.mark.asyncio
-async def test_entrypoint_call_performance(
-    entrypoint: Entrypoint[User, Greeting],
-) -> None:
+async def test_call_performance(entrypoint: Entrypoint[User, Greeting]) -> None:
     input_messages = [User(name=f"{i}") for i in range(1000)]
 
     output_message = await entrypoint.call(
@@ -90,3 +89,55 @@ async def test_entrypoint_call_performance(
     ):
         assert output_message.name == input_message.name
         assert output_message.greeting == f"Hello, {input_message.name}!"
+
+
+@pytest.mark.asyncio
+async def test_process_performance(
+    messaging: MessagingABC,
+    user_topic: TopicABC[User],
+    greeting_topic: TopicABC[Greeting],
+    _entrypoint: Entrypoint[User, Greeting],
+) -> None:
+    input_messages = [User(name="Cara"), User(name="Greg")]
+    input_headers = list()
+    for input_message in input_messages:
+        input_header = user_topic.header_type(correlation_id=uuid4())
+        input_header.message = input_message
+        input_headers.append(input_header)
+
+    async with messaging:
+        await user_topic.subscribe(_entrypoint.name)
+        await greeting_topic.subscribe("test")
+        await user_topic.send_batch(input_headers)
+
+        start = timer()
+        await _entrypoint.process(batch_size=2)
+        end = timer()
+
+        diff = end - start
+        logger.info(f"{diff}s elapsed")
+        logger.info("{} messages / s".format(1000 / diff))
+
+        correlation_ids = set(
+            input_header.correlation_id for input_header in input_headers
+        )
+        async with _entrypoint.reply_topic.receive_batch(
+            "test", "test", batch_size=2
+        ) as output_headers:
+            for output_header in output_headers:
+                if output_header.correlation_id in correlation_ids:
+                    correlation_ids.remove(output_header.correlation_id)
+
+                for input_header in input_headers:
+                    if output_header.correlation_id == input_header.correlation_id:
+                        assert not output_header.error
+                        assert output_header.message
+                        assert input_header.message
+                        assert output_header.message.name == input_header.message.name
+                        assert (
+                            output_header.message.greeting
+                            == f"Hello, {input_header.message.name}!"
+                        )
+        assert len(correlation_ids) == 0
+
+    pass
